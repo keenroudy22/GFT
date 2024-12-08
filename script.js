69f34cf23c67cba706e869d657db358e67e44efc
keenroudy22 <kroudebush96@gmail.com>
2024-12-08 12:15:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ document.getElementById('order-form').addEventListener('submit', function(event)
     event.preventDefault();
 
     const formData = new FormData(event.target);
-    const data = {};
-    formData.forEach((value, key) => { data[key] = value; });
+    const data = new URLSearchParams();
+
+    // Convert FormData to URLSearchParams for correct formatting
+    formData.forEach((value, key) => { data.append(key, value); });
 
     fetch('https://script.google.com/macros/s/1_Gzrk__JgiShntZqFJH4xmuKJt8SH3ElTZiegPhPAAg/exec', {
         method: 'POST',
-        body: JSON.stringify(data),
-        headers: { 'Content-Type': 'application/json' }
+        body: data,
     })
     .then(response => response.json())
     .then(result => {
